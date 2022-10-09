382403f7dde5ae185d394ee5aa6b316569160473
ending2015a <joehsiao@gapp.nthu.edu.tw>
2022-10-09 04:48:53 +0000

    fixed messenger in-app browser failed issue

--- src/app.py
+++ src/app.py
@@ -1,23 +1,32 @@
 # --- built in ---
 import os
 import io
-import tempfile
+import time
+import uuid
 import subprocess
 from typing import List
 # --- 3rd party ---
 import PIL.Image
+PIL.Image.MAX_IMAGE_PIXELS = 10000000000
 from fastapi import FastAPI, UploadFile
-from fastapi.responses import FileResponse, HTMLResponse
+from fastapi.responses import FileResponse, HTMLResponse, RedirectResponse
 from starlette.background import BackgroundTask
+import starlette.status as status
 # --- my module ---
 import gen
 
 
 app = FastAPI()
 
-def remove_file(*paths: List[str]) -> None:
+def remove_file_after_n_sec(sec: int, *paths: List[str]) -> None:
+  time.sleep(sec)
   for path in paths:
     os.unlink(path)
+
+@app.get("/")
+async def redirect():
+  return RedirectResponse("/upload")
+
 
 @app.get(
   "/upload",
@@ -48,7 +57,7 @@ async def upload_form():
         <h1>Secret Converter.</h1>
         <h5>Upload some images and to see what will happen....</h5>
         <div class="container">
-          <form action="api/upload" enctype="multipart/form-data" method="post" autocomplete="off">
+          <form action="/api/upload" enctype="multipart/form-data" method="post" autocomplete="off">
             <div class="row justify-content-center">
               <div class="input-group col-md-4">
                 <input class="form-control" type="file" id="file" name="file">
@@ -77,25 +86,37 @@ async def upload_file(file: UploadFile):
   user_filename = user_filename[:10]
   user_filename = user_filename + '.usdz'
 
-  file.file.seek(0)
   image = PIL.Image.open(io.BytesIO(file.file.read()))
 
   width, height = image.size
   canvas = gen.gen_canvas(width=width, height=height)
   model_bytes = gen.gen_model(canvas, image)
   # save temporary glb files
-  with tempfile.NamedTemporaryFile(suffix='.glb', delete=False) as f:
-    glb_filename = f.name
-    usd_filename = os.path.splitext(glb_filename)[0] + '.usdz'
+  file_id = str( uuid.uuid4())
+  glb_filename = file_id + '.glb'
+  usd_filename = file_id + '.usdz'
+  with open(glb_filename, 'wb') as f:
     f.write(model_bytes)
   subprocess.run(["usd_from_gltf", glb_filename, usd_filename])
 
-  return FileResponse(
-    path = usd_filename,
-    media_type = 'model/usd',
-    filename = user_filename,
-    background = BackgroundTask(remove_file, glb_filename, usd_filename))
+  return RedirectResponse(
+    f"/download/{file_id}",
+    status_code=status.HTTP_302_FOUND,
+    background = BackgroundTask(remove_file_after_n_sec, 60, glb_filename, usd_filename)
+  )
 
+
+@app.get('/download/{file_id}')
+async def downloa_file(file_id: str):
+  usd_filename = os.path.basename(file_id) + '.usdz'
+  if os.path.isfile(usd_filename):
+    return FileResponse(
+      path = usd_filename,
+      media_type = 'model/usd',
+      filename = usd_filename
+    )
+  else:
+    return {'error': 'file not found'}
 
 
 if __name__ == '__main__':
--- start_server.sh
+++ start_server.sh
@@ -6,5 +6,5 @@ docker run --rm \
     -p 8000:8000 \
     --entrypoint "/bin/bash" \
     $DOCKER_NAME \
-    -c "cd src/ ; uvicorn app:app --port 8000 --host 0.0.0.0"
+    -c "cd src/ ; uvicorn app:app --reload --port 8000 --host 0.0.0.0"
 
